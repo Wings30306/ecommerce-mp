a4a554a1ba3ab864085ae79db2bb4c1cb7cfda31
Joke Heyndels <jo_hannah@outlook.com>
2019-04-16 14:58:46 +0200

    Add search functionality

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,6 +20,7 @@ from accounts import urls as urls_accounts
 from products import urls as urls_products
 from products.views import all_products
 from cart import urls as urls_cart
+from search import urls as urls_search
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -29,6 +30,7 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products)),
     url(r'^cart/', include(urls_cart)),
-    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
+    url(r'^search/', include(urls_search)),
 
 ]
